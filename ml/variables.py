fe97992bf84ee6512b1b698c42a6f4a613783720
EKaterina Tsareva <ekaterinatsr@gmail.com>
2024-09-23 13:55:59 +0300

    fix keys

--- ml/variables.py
+++ ml/variables.py
@@ -2,11 +2,14 @@ CA = './CA.pem' # если вы не меняли путь при скачива
 VERIFY = True
 SECURE = True
 USER = 'admin'
-PASSWORD = 'hakaton1'
-HOST = "rc1d-uq593a42e02ikh2k.mdb.yandexcloud.net" # адрес Clickhouse
+PASSWORD = '5U?H.5,F-@D$p2b'
+HOST = "rc1d-3lv6d1bous4laj3t.mdb.yandexcloud.net" # адрес Clickhouse
 PORT = 8443
-folder = 'b1g01stbeeo96j8kfaiv'
-service_account_id = "aje286em9u4ibs8572di"
-key_id = 'ajeo9id7551hpq15fv3o'
+
+
+folder = 'b1gkt5jnf1leugqln1r2'
+service_account_id = "ajeun4aue16or4mk4lta"
+key_id = "ajeefvp6s70c4aipdr84"
 model_uri = f'gpt://{folder}/yandexgpt/latest'
-token_m = '' #add token
+
+key_api = 'sk-kllZ7MZAyhbzNJVpWfT7SPxDtI2jkJXi'
